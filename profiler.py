588d3b20519c48c599c779a8f8234cd5a60fb4b4
Ben Simner <benjsimner@gmail.com>
2017-02-12 00:12:14 +0000

    Clean up profiler

--- profiler.py
+++ profiler.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
 import cProfile
 import pstats
-import contextlib
-import io
 import pathlib
 import pytest
 import matplotlib.pyplot as plt
 from pprint import pprint
 
+# profile the test suite
 p = cProfile.Profile()
 p.enable()
 pytest.main(['tests/'])
 p.disable()
 
-sio = io.StringIO()
 
+# this is the nicest way to collect the stats from above
+# as far as i can tell
 stats = pstats.Stats()
-stats.load_stats(p.create_stats())
-
-# this is the nicest way to interact between the two
 p.dump_stats('profile')
 stats.add('profile')
 
+# get the top 10% of speccer's runtime
 stats.sort_stats('tottime')
 w, st = stats.get_print_list(['speccer/speccer',0.1])
 labels = []
@@ -31,18 +29,25 @@ T = 0
 for f in st:
     T += stats.stats[f][2]
 
+other = T*0.1
+T = T + other
+
 for f in st:
     mname, line, fname = f
     mname_path = pathlib.Path(mname)
-    mname = mname_path.parts[-1]
+    module = mname_path.parts[-1]
     stat = stats.stats[f]
     prim_call_count, num_calls, tot_time, cum_time, callers = stat
-    name = f'{mname}:{fname}({line})'
+    name = f'{module}:{fname}(line {line})'
     labels.append(name)
     sizes.append(tot_time/T)
 
+# account for "other"
+labels.append('other')
+sizes.append(other/T)
+
+# plot the stats
 fig, ax = plt.subplots()
 ax.pie(sizes, labels=labels)
 ax.axis('equal')
-
 plt.show()
