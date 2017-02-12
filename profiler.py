55d69fa4f8fe508dcfb2e4e0dd46d664233623bc
Ben Simner <benjsimner@gmail.com>
2017-02-12 01:14:41 +0000

    More advanced profiler

--- profiler.py
+++ profiler.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+import os
 import cProfile
 import pstats
 import pathlib
 import pytest
 import matplotlib.pyplot as plt
 from pprint import pprint
+
+PROFILE_FILE = '.profile'
 
 # profile the test suite
 p = cProfile.Profile()
@@ -16,38 +19,66 @@ p.disable()
 # this is the nicest way to collect the stats from above
 # as far as i can tell
 stats = pstats.Stats()
-p.dump_stats('profile')
-stats.add('profile')
+p.dump_stats(PROFILE_FILE)
+stats.add(PROFILE_FILE)
 
 # get the top 10% of speccer's runtime
 stats.sort_stats('tottime')
-w, st = stats.get_print_list(['speccer/speccer',0.1])
-labels = []
-sizes  = []
+w, st = stats.get_print_list(['speccer/speccer', 0.1])
 
-T = 0
-for f in st:
-    T += stats.stats[f][2]
+def make_pie_from_callers(callers, rem=0):
+    labels, sizes, callbacks = [], [], {}
 
-other = T*0.1
-T = T + other
+    T = sum(stats.stats[f][2] for f in callers)
+    other = T*rem
+    T = T + other
 
-for f in st:
-    mname, line, fname = f
-    mname_path = pathlib.Path(mname)
-    module = mname_path.parts[-1]
-    stat = stats.stats[f]
-    prim_call_count, num_calls, tot_time, cum_time, callers = stat
-    name = f'{module}:{fname}(line {line})'
-    labels.append(name)
-    sizes.append(tot_time/T)
+    for f in callers:
+        mname, line, fname = f
+        mname_path = pathlib.Path(mname)
+        module = mname_path.parts[-1]
+        stat = stats.stats[f]
+        prim_call_count, num_calls, tot_time, cum_time, callers = stat
+        name = f'{module}:{fname}(line {line})'
+        labels.append(name)
+        sizes.append(tot_time/T)
+        callbacks[name] = callers
 
-# account for "other"
-labels.append('other')
-sizes.append(other/T)
+    if other != 0:
+        # account for "other"
+        labels.append('other')
+        sizes.append(other/T)
+        callbacks['other'] = None
 
-# plot the stats
-fig, ax = plt.subplots()
-ax.pie(sizes, labels=labels)
-ax.axis('equal')
+    return labels, sizes, callbacks
+
+figs = {}
+
+def make_new_pie_from_callers(callers, call_name=None, rem=0):
+    print('make new pie from callers({})'.format(callers))
+    # plot the stats
+    fig, ax = plt.subplots()
+
+    if call_name:
+        ax.set_title('Breakdown of {} callees'.format(call_name))
+
+    labels, sizes, callbacks = make_pie_from_callers(callers, rem=rem)
+    wedges, _ = ax.pie(sizes, labels=labels)
+
+    for w in wedges:
+        w.set_picker(True)
+
+    def onclick(evt):
+        l = evt.artist.get_label()
+        cb = callbacks[l]
+        if cb:
+            make_new_pie_from_callers(cb, call_name=l)
+
+
+    fig.canvas.mpl_connect('pick_event', onclick)
+    ax.axis('equal')
+    plt.show()
+
+make_new_pie_from_callers(st, rem=0.1)
+os.remove(PROFILE_FILE)
 plt.show()
